f96631a9bc5a9012134b183fb2738ac78b6335cc
Sneha-S-D <snehaastrophile@gmail.com>
2025-08-18 21:14:42 +0530

    model training added

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -2,15 +2,17 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from cnnClassifier import logger
  
-STAGE_NAME ="Prepare base model"
+STAGE_NAME = "Prepare base model"
 
 class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        prepare_base_model_config = config.get_prepare_base_model_config()
+        config_manager = ConfigurationManager()
+        prepare_base_model_config = config_manager.get_prepare_base_model_config()
+        
+        # Use the config object correctly
         prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
